20607c1f54adf3a6c70f8c94eb64f93059416a86
crytek12 <89571912+crytek12@users.noreply.github.com>
2023-01-07 01:32:45 +0530

    Add files via upload

---
+++ Python Advanced/Personal Resources/test2.py
@@ -0,0 +1,13 @@
+def test(a, b):
+    return a + b 
+    
+def test1(b, c):
+    """This is a function which can return a mul of 2 num"""
+    return b * c
+    
+def test3(b, n):
+    return b / n 
+    
+def test4():
+    print('this is my code written in module test2')
+ 
